92f23892a6e3603ae0b7cac75581f1aa7ba1e8e7
Tomas Vidal <tomi_vidal_99@hotmail.com>
2022-05-07 10:59:52 -0300

    Fixed that idk why some lines where missing in the i3 config, and added the disown command to the tray.sh

--- .utility/i3/tray.sh
+++ .utility/i3/tray.sh
@@ -4,6 +4,6 @@ pkill -f pasystray
 pkill -f blueman-tray
 pkill -f nm-applet
 
-pasystray --notify=all &
-blueman-tray &
-nm-applet --indicator &
+pasystray --notify=all & disown
+blueman-tray & disown
+nm-applet --indicator & disown
