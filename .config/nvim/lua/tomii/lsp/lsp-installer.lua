3788aff0bd917595763ed09b2e2036f357d34893
Tomás Vidal <tomi_vidal_99@hotmail.com>
2022-11-20 10:40:12 -0300

    Upated from dotfiles_desktop

--- .config/nvim/lua/tomii/lsp/lsp-installer.lua
+++ .config/nvim/lua/tomii/lsp/lsp-installer.lua
@@ -1,5 +1,11 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
+  print "ERROR: nvim-lsp-installer not available. Called from lsp-installer.lua"
+  return
+end
+local lsp_configs_ok, lsp_configs = pcall(require, "lspconfig/configs")
+if not lsp_configs_ok then
+  print "ERROR: lspconfig/configs not available. Called from lsp-installer.lua"
   return
 end
 
@@ -19,9 +25,15 @@ lsp_installer.setup({
   }
 })
 
+-- Error that throws clangd
+capabilities.offsetEncoding = "utf-8"
+
 -- Define locallly the lsp
 local lsp = require("lspconfig")
 local lsps_opts = {on_attach = on_attach, capabilities = capabilities}
+
+-- Server for cpp/c
+lsp.clangd.setup(lsps_opts)
 
 -- Server for javascript, typescript, react javascript and react typescript.
 lsp.tsserver.setup(lsps_opts)
@@ -35,15 +47,11 @@ lsp.tailwindcss.setup(lsps_opts)
 -- For html kinda of snippets
 lsp.emmet_ls.setup(lsps_opts)
 
-
 -- For css, scss and less
 lsp.cssls.setup(lsps_opts)
 
 -- Python
 lsp.pyright.setup(lsps_opts)
-
--- JSON
-lsp.jsonls.setup(lsps_opts)
 
 -- Server language for lua.
 lsp.sumneko_lua.setup({
@@ -52,21 +60,13 @@ lsp.sumneko_lua.setup({
   settings = require("tomii.lsp.settings.sumneko_lua"),
 })
 
--- C#
-local pid = vim.fn.getpid()
-local omnisharp_executable = "omnisharp"
-lsp.omnisharp.setup({
-  -- TODO: move all this config to a different directory
-  capabilities = capabilities,
-  on_attach = on_attach,
-  cmd = { omnisharp_executable, "--languageserver" , "--hostPID", tostring(pid) },
-  enable_editorconfig_support = true,
-  enable_ms_build_load_projects_on_demand = false,
-  enable_roslyn_analyzers = false,
-  organize_imports_on_format = false,
-  enable_import_completion = false,
-  sdk_include_prereleases = true,
-  analyze_open_documents_only = false,
-})
+-- CUSTOM LSPS
 
--- TODO: VHDL language server
+-- mlang
+-- vim.lsp.start({
+--   name = "mlang",
+--   cmd = {"/home/tomii/programming/lsp_mlang/run.sh"},
+--   filetypes = { "matlab", "octave" },
+--   root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
+--   settings = {},
+-- })
