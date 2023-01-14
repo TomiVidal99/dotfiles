aa2dc4c40fd10beb40b59b81066bceb59c6644b0
Tomás Vidal <tomi_vidal_99@hotmail.com>
2023-01-14 09:07:35 -0300

    Changed theme and updated to most recent changes of the plugin

--- .config/nvim/lua/tomii/utils/lspsaga.lua
+++ .config/nvim/lua/tomii/utils/lspsaga.lua
@@ -6,9 +6,35 @@ if not status_ok then
 end
 
 lspsaga.setup({
-	saga_winblend = 1,
-	symbol_in_winbar = {
-		in_custom = true,
+	ui = {
+		-- currently only round theme
+		theme = "round",
+		-- border type can be single,double,rounded,solid,shadow.
+		border = "solid",
+		winblend = 20,
+		expand = "",
+		collapse = "",
+		preview = " ",
+		code_action = " ",
+		diagnostic = "🪲",
+		incoming = " ",
+		outgoing = " ",
+		colors = {
+			--float window normal background color
+			normal_bg = "#002b36",
+			--title background color
+			title_bg = "#255667",
+			red = "#dc322f",
+			magenta = "#d33682",
+			orange = "#cb4b16",
+			yellow = "#f7bb3b",
+			green = "#719e07",
+			cyan = "#2aa198",
+			blue = "#268bd2",
+			purple = "#6c71c4",
+			white = "#d1d4cf",
+			black = "#1c1c19",
+		},
+		kind = {},
 	},
-	code_action_icon = " ",
 })
