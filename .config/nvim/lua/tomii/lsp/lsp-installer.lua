9404bccde1c2452687ef937fa7b8c26dc35737bb
Tomás Vidal <32806650+TomiVidal99@users.noreply.github.com>
2023-01-26 18:37:14 -0300

    Features (#8)
    * Upated from dotfiles_desktop
    
    * Minor changes
    
    * Added script to get all pdf in 'documents_directories' and open them with rofi
    
    * Removed discord at startup
    
    * Updated get_documents from dotfiles_desktop
    
    * fixed documents path
    
    * Removed i3/config from dotfiles
    
    * added alias for add_common_directory function 'acd'
    
    * Updated where octave plots goes and changed names of workspaces for emojis
    
    * Improved functions and added acd to add launch_directories
    
    * Fixed some minor typos
    
    * Improved function, now considers working directories as well
    
    * Changed tab to switch inside completion to Ctrl+(j/k)
    
    * Added modelsim to ws 14 and updated web icon to a world
    
    * Added rust_hdl as LSP for vhd
    
    * tailwindcss config conflicts with rust_hdl
    
    * Changed mode to manual
    
    * Added videos directories to files_directories
    
    * Added hover_doc with Lspsaga
    
    * Minor change
    
    * changed spice to have the 11 workspace
    
    * Working on nofications and volume update
    
    * Made script to update volume
    
    * Made script to update volume
    
    * Made script to change volume
    
    * Removed unnecessary scrip
    
    * New aliases for git
    
    * Added new shortcuts for git
    
    * Added lsp for .sh files
    
    * Just minor styling changes
    
    * Updated lspsaga and its config, also added documentation generator doge
    
    * Added LSP loading display, fidget
    
    * Changed theme and updated to most recent changes of the plugin
    
    * Updated theme of the winbar
    
    * Minor theme changes
    
    * Minor change
    
    * Added automatic LSP setup with mason-lspconfig
    
    * Added auto setup LPSs with mason-lspconfig
    
    * Minor improvements
    
    * Minor changes
    
    * Improved tabs and tabline
    
    * Refactor
    
    * Added vim-fugitive
    
    * Fixed switch_working_directory, now it wont try to switch to multiple ones
    
    * Made the tabline prettier but there's a bad config that display the tabs in the wrong order, gotta FIX that
    
    * Added [LSP] descriptor when completion pops up from LSP
    
    * Minor changes
    
    * Changed bumblestatus for i3status-rust
    
    * changed bumblebee-status for i3status-rust, more performant
    
    * Added bluetooth module for headphones
    
    * Added some i3status-rust block modules, network, weather and bluetooth
    
    * Changed position of modules
    
    * Improved colors and icons, also added pacman block

--- .config/nvim/lua/tomii/lsp/lsp-installer.lua
+++ .config/nvim/lua/tomii/lsp/lsp-installer.lua
@@ -1,98 +1,62 @@
-local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
-if not status_ok then
-  print "ERROR: nvim-lsp-installer not available. Called from lsp-installer.lua"
-  return
-end
-local lsp_configs_ok, lsp_configs = pcall(require, "lspconfig/configs")
-if not lsp_configs_ok then
-  print "ERROR: lspconfig/configs not available. Called from lsp-installer.lua"
-  return
-end
+local lsp_installer = require("nvim-lsp-installer")
+local lspconfig = require("lspconfig")
+
+-- Register a handler that will be called for all installed servers.
+lsp_installer.setup({
+	automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
+	ui = {
+		icons = {
+			server_installed = "✓",
+			server_pending = "➜",
+			server_uninstalled = "✗",
+		},
+	},
+})
 
 -- Import options from the other files
 local on_attach = require("tomii.lsp.handlers").on_attach
 local capabilities = require("tomii.lsp.handlers").capabilities
+local lsps_opts = { on_attach = on_attach, capabilities = capabilities }
 
--- Register a handler that will be called for all installed servers.
-lsp_installer.setup({
-  automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-  ui = {
-    icons = {
-      server_installed = "✓",
-      server_pending = "➜",
-      server_uninstalled = "✗"
-    }
-  }
-})
+-- LPSs
+local lsp_list = {
+  "clangd",
+  "tsserver",
+  "diagnosticls",
+  "tailwindcss",
+  "emmet_ls",
+  "cssls",
+  "pyright",
+  "jsonls"
+}
 
--- Error that throws clangd
-capabilities.offsetEncoding = "utf-8"
+for _, lsp in pairs(lsp_list) do
+  lspconfig[lsp].setup(lsps_opts)
+end
 
--- Define locallly the lsp
-local lsp = require("lspconfig")
-local lsps_opts = {on_attach = on_attach, capabilities = capabilities}
-
--- Server for cpp/c
-lsp.clangd.setup(lsps_opts)
-
--- Server for javascript, typescript, react javascript and react typescript.
-lsp.tsserver.setup(lsps_opts)
-
--- General diagnostics for most languages
-lsp.diagnosticls.setup(lsps_opts)
-
--- Tailwindcss support, shows colors and completion
-lsp.tailwindcss.setup(lsps_opts)
-
--- For html kinda of snippets
-lsp.emmet_ls.setup(lsps_opts)
-
--- For css, scss and less
-lsp.cssls.setup(lsps_opts)
-
--- Python
-lsp.pyright.setup(lsps_opts)
-
--- JSON
-lsp.jsonls.setup(lsps_opts)
-
+-- TODO: think how to change the lsp_list to accept custom settings
 -- LUA
-lsp.sumneko_lua.setup({
+lspconfig.sumneko_lua.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   settings = require("tomii.lsp.settings.sumneko_lua"),
 })
 
--- VHDL
-local lspconfig = require'lspconfig'
--- Only define once
-if not lspconfig.hdl_checker then
-  require'lspconfig/configs'.hdl_checker = {
-    default_config = {
-    cmd = {"hdl_checker", "--lsp", };
-    filetypes = {"vhdl", "verilog", "systemverilog"};
-      root_dir = function(fname)
-        -- will look for a parent directory with a .git directory. If none, just
-        -- use the current directory
-        --return lspconfig.util.find_git_ancestor(fname) or lspconfig.util.path.dirname(fname)
-        -- or (not both)
-        -- Will look for the .hdl_checker.config file in a parent directory. If
-        -- none, will use the current directory
-        return lspconfig.util.root_pattern('.hdl_checker.config')(fname) or lspconfig.util.path.dirname(fname)
-      end;
-      settings = {};
-    };
-  }
+-- Custom LSPs
+local custom_lsps = {
+  "rust_hdl",
+  "bash_language_server",
+  "hdl_checker"
+}
+for _, lsp in pairs(custom_lsps) do
+  lspconfig[lsp].setup(lsps_opts)
 end
 lsp.hdl_checker.setup(lsps_opts)
 
--- CUSTOM LSPS
-
--- mlang
--- vim.lsp.start({
---   name = "mlang",
---   cmd = {"/home/tomii/programming/lsp_mlang/run.sh"},
---   filetypes = { "matlab", "octave" },
---   root_dir = vim.fs.dirname(vim.fs.find({'setup.py', 'pyproject.toml'}, { upward = true })[1]),
---   settings = {},
--- })
+-- mason-lspconfig allows me to automatically configure all installed LPSs
+-- Though the configs must exists in the lspconfig.
+require("mason-lspconfig").setup_handlers({
+	function(server_name) -- default handler (optional)
+		lspconfig[server_name].setup(lsps_opts)
+	end,
+})
