ef5bfc80e0113484f76e32d23d8d18166e0f8d31
Tomás Vidal <tomividal9@gmail.com>
2024-01-30 13:39:58 -0300

    Added rofi and search documents with rofi

--- .utility/common/get_documents.sh
+++ .utility/common/get_documents.sh
@@ -1,35 +1,37 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Gets the documents listed in the 'documents_dir'
 # file and lets you (with rofi) open them in your file manager
 
+#alias okular='"/mnt/c/Program Files/Okular/bin/okular.exe"'
 
 DIRECTORIES=~/.dotfiles/.utility/common/documents_directories
 
 # opens rofi with the right flags
-function rofi_command() {
+rofi_command() {
   rofi -dmenu -i -markup -markup-rows -p "Seleccione un documento"
 }
 
 # returns the line parsed with the data
-function get_options_line() {
+get_options_line() {
   name=$(basename -s pdf "$1")
   printf "<big><b>%s</b></big> - <small><i>%s</i></small>\x00icon\x1fapplication-pdf\n" "$name" "$1"
 }
 
-export -f get_options_line
-
 # feeds all the one line options to the line creator
-function parse_options() {
-  xargs -I '{}' bash -c 'get_options_line "{}"'
+parse_options() {
+  #xargs -I '{}' zsh -c 'get_options_line "{}"'
+  xargs -I '{}' zsh -c 'get_options_line() { name=$(basename -s pdf "$1"); printf "<big><b>%s</b></big> - <small><i>%s</i></small>\x00icon\x1fapplication-pdf\n" "$name" "$1"; }; get_options_line "{}"'
 }
 
 # extracts the path from the given return value
-function get_path_from_selected_option() {
+get_path_from_selected_option() {
   sed -E "s/.*<i>//" | sed -E "s/<\/i>.*//"
 }
 
-DOCUMENTS=$(cat $DIRECTORIES | xargs -I '{}' fd -e pdf . {})
+DOCUMENTS=$(cat $DIRECTORIES | xargs -I '{}' fdfind -e pdf . "{}")
+okular "/mnt/c/Users/tomiv/My Documents/facultad/Circuitos y Sistemas Lineales/PARCIALES MOD 1/comprobante-1.pdf"
+exit
 SELECTED_DOCUMENT_NAME=$(echo "$DOCUMENTS" | parse_options | rofi_command)
 SELECTED_DOCUMENT_PATH=$(echo "$SELECTED_DOCUMENT_NAME" | get_path_from_selected_option)
 
@@ -40,4 +42,5 @@ if [[ "$SELECTED_DOCUMENT_NAME" = "" || "$SELECTED_DOCUMENT_PATH" = "" ]]; then
   exit
 fi
 
-xdg-open "$SELECTED_DOCUMENT_PATH"
+echo "selected document: '$SELECTED_DOCUMENT_PATH'"
+okular """$SELECTED_DOCUMENT_PATH"""
--- .utility/i3/execute_rofi.sh
+++ .utility/i3/execute_rofi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-rofi -show drun -modi "drun,run,filebrowser,emoji:/usr/bin/rofimoji" -show emoji -showicons -matching fuzzy
+#rofi -show drun -modi "drun,run,filebrowser,emoji:/usr/bin/rofimoji" -show emoji -showicons -matching fuzzy
+rofi -show drun -modi "drun,run,filebrowser" -show -showicons -matching fuzzy
